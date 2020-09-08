04a189474d9e7e3d3f62631a934738cfbaef97fc
Joshua Medlen <joshua246@gmail.com>
2020-09-08 18:41:11 -0500

    added a footer

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.scss';
 import MyNavbar from '../components/pages/MyNavbar/MyNavbar';
+import MyFooter from '../components/pages/MyFooter/MyFooter';
 
 class App extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class App extends React.Component {
       <div className="App">
         <MyNavbar />
         <h2>Personal Bio Site</h2>
+        <MyFooter />
       </div>
     );
   }
