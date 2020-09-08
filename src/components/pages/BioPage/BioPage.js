f553cd2c63d490e53aa5688d190496fd594f5094
Joshua Medlen <joshua246@gmail.com>
2020-09-08 18:35:07 -0500

    added basic content to pages to show pathing is working

---
+++ src/components/pages/BioPage/BioPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class BioPage extends React.Component {
+  render() {
+    return (
+      <div className="BioPage">
+        <h3>Bio</h3>
+      </div>
+    );
+  }
+}
+
+export default BioPage;
---
+++ src/components/pages/TechPage/TechPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TechPage extends React.Component {
+  render() {
+    return (
+      <div className="Technologies">
+        <h3>Technologies</h3>
+      </div>
+    );
+  }
+}
+
+export default TechPage;
