ff32c1b5e33711350eb5b71d7c93a8f8e59b4d65
Joshua Medlen <joshua246@gmail.com>
2020-09-08 19:01:34 -0500

    throwing error saying possible infinite loop?

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,44 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import './App.scss';
 import MyNavbar from '../components/pages/MyNavbar/MyNavbar';
 import MyFooter from '../components/pages/MyFooter/MyFooter';
+import BioPage from '../components/pages/BioPage/BioPage';
+import Home from '../components/pages/Home/Home';
+import ProjectsPage from '../components/pages/ProjectsPage/ProjectsPage';
+import TechPage from '../components/pages/TechPage/TechPage';
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/home', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <MyNavbar />
-        <h2>Personal Bio Site</h2>
-        <MyFooter />
+        <BrowserRouter>
+          <React.Fragment>
+            <MyNavbar />
+            <div className="container">
+              <Switch>
+                <PublicRoute path="/home" component={Home} />
+                <PublicRoute path="/home" component={BioPage} />
+                <PublicRoute path="/home" component={ProjectsPage} />
+                <PublicRoute path="/home" component={TechPage} />
+                <Redirect from="*" to="/home" />
+              </Switch>
+            </div>
+            <MyFooter />
+          </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }
