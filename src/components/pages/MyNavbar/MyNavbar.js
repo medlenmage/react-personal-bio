3356becb0ad98f7f8cea58949dbd5e85ac5cc6c0
Joshua Medlen <joshua246@gmail.com>
2020-09-27 14:32:09 -0500

    did some styling to site overall. fixed up navbar. looking into font for site

--- src/components/pages/MyNavbar/MyNavbar.js
+++ src/components/pages/MyNavbar/MyNavbar.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './MyNavbar.scss';
 
 class MyNavbar extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/home">Joshua Medlen</a>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="/about-me">Bio</a>
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <Link className="navbar-brand links" to="/home">Joshua Medlen</Link>
+        <div className="link-list-container">
+          <ul className="navbar-nav link-list">
+            <li className="nav-item">
+              <Link className="nav-link links" to="/about-me">About Me</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/Technologies">Tech</a>
+              <Link className="nav-link links" to="/Technologies">Tech</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/Projects">Projects</a>
+              <Link className="nav-link links" to="/Projects">Projects</Link>
             </li>
           </ul>
         </div>
