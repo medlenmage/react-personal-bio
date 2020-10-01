835f6ac69293ba64b85d259fef86e2c11324fe6a
Joshua Medlen <joshua246@gmail.com>
2020-10-01 16:17:13 -0500

    got email added to footer. will fix styling of it later to get links horizontal.

--- src/components/pages/MyFooter/MyFooter.js
+++ src/components/pages/MyFooter/MyFooter.js
@@ -8,10 +8,9 @@ class MyFooter extends React.Component {
           <nav className="navbar navbar-light mt-auto">
             <div className="link-list-container">
               <ul className="navbar-nav link-list">
-                {/* <li className="nav-item email">
-                  <Link to="joshua246@gmail.com">joshua246@gmail.com</Link>
-                  <a href="joshua246@gmail.com">Joshua246@gmail.com</a>
-                </li> */}
+                <li className="nav-item email">
+                  <a href="mailto:joshua246@gmail.com" className="links">Joshua246@gmail.com</a>
+                </li>
                 <li className="nav-item footer-listed">
                   <a href="https://www.linkedin.com/in/joshuamedlen/" className="links">https://www.linkedin.com/in/joshuamedlen/</a>
                 </li>
