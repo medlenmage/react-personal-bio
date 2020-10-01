a64c743349e1ee19f28d74697ca511db60943c13
Joshua Medlen <joshua246@gmail.com>
2020-10-01 16:04:02 -0500

    messed with the footer a little will come back to it later gonna start my projects portion of the page

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {
   BrowserRouter,
-  Redirect,
+  // Redirect,
   Route,
   Switch,
 } from 'react-router-dom';
 import './App.scss';
 import MyNavbar from '../components/pages/MyNavbar/MyNavbar';
-// import MyFooter from '../components/pages/MyFooter/MyFooter';
+import MyFooter from '../components/pages/MyFooter/MyFooter';
 import BioPage from '../components/pages/BioPage/BioPage';
 import Home from '../components/pages/Home/Home';
 import ProjectsPage from '../components/pages/ProjectsPage/ProjectsPage';
@@ -26,10 +26,10 @@ class App extends React.Component {
                 <Route path="/About-Me" component={BioPage} />
                 <Route path="/Projects" component={ProjectsPage} />
                 <Route path="/Technologies" component={TechPage} />
-                <Redirect from="*" to="/home" />
+                {/* <Redirect from="*" to="/home" /> */}
               </Switch>
             </div>
-            {/* <MyFooter /> */}
+            <MyFooter />
           </React.Fragment>
         </BrowserRouter>
       </div>
