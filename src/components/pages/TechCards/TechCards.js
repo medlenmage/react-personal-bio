b1f3683580c1a1b8a48e2b0de3e46c3c4f68467f
Joshua Medlen <joshua246@gmail.com>
2020-10-03 16:30:45 -0500

    almost finished with tech page. says it is rendering incorrectly. expects string but is getting an object.

---
+++ src/components/pages/TechCards/TechCards.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import techShape from '../../../helpers/propz/techShape';
+
+class TechCards extends React.Component {
+  static propTypes = {
+    tech: techShape.techShape,
+  }
+
+  render() {
+    const { tech } = this.props;
+
+    return (
+      <div className="card">
+        <img className="card" src={tech.icon} alt="tech-icon"/>
+        <div className="card-body">
+          <h5 className="card-title header">{tech.name}</h5>
+          <p className="card-text p">{tech.description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default { TechCards };
--- src/components/pages/TechPage/TechPage.js
+++ src/components/pages/TechPage/TechPage.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import techData from '../../../helpers/data/techData';
+import TechCards from '../TechCards/TechCards';
 
 class TechPage extends React.Component {
+  state = {
+    techs: [],
+  }
+
+  componentDidMount() {
+    techData.getTech()
+      .then((techs) => this.setState({ techs }))
+      .catch((err) => console.error('could not get tech', err));
+  }
+
   render() {
+    const { techs } = this.state;
+
+    const techCard = techs.map((tech) => <TechCards key={tech.id} techs={tech} />);
+
     return (
       <div className="Technologies">
         <h3>Technologies</h3>
+        <div className="m-auto">
+          <div className="card-columns">
+            {techCard}
+          </div>
+        </div>
       </div>
     );
   }
